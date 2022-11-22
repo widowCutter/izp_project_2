f2387c710d4534f951cc47e3ad10f98e2a98f36d
widowCutter <ondrej@pribyl.tech>
2022-11-22 22:53:33 +0100

    reformat

--- cluster.c
+++ cluster.c
@@ -1,33 +1,31 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #define err -1
 #define success 0
-struct Object{
+struct Object {
   int id;
   int x;
   int y;
 };
-struct Matrix{
+struct Matrix {
   struct Object *objects;
   int size;
 };
-void print_matrix_cli(struct Matrix *matrix)
-{
+void print_matrix_cli(struct Matrix *matrix) {
   for (int i = 0; i < matrix->size; i++) {
-    printf("ID:[%d] x:[%d], y:[%d]\n", matrix->objects[i].id,matrix->objects[i].x,matrix->objects[i].y);
+    printf("ID:[%d] x:[%d], y:[%d]\n", matrix->objects[i].id,
+           matrix->objects[i].x, matrix->objects[i].y);
   }
 }
-FILE *init_file(char *path)
-{
+FILE *init_file(char *path) {
   FILE *fp = fopen(path, "r");
   return fp;
 }
-int parse_table(struct Matrix *matrix, FILE *fp)
-{
+int parse_table(struct Matrix *matrix, FILE *fp) {
   int count;
-  fscanf(fp,"count=%d", &count);
+  fscanf(fp, "count=%d", &count);
   printf("%d\n", count);
   matrix->size = count;
   if ((matrix->objects = malloc(sizeof(struct Object) * count)) == NULL) {
@@ -41,25 +39,25 @@ int parse_table(struct Matrix *matrix, FILE *fp)
   }
   return success;
 }
-int cluster_matrix(int num_clusters, struct Matrix *matrix)
-{
+int cluster_matrix(int num_clusters, struct Matrix *matrix) {
   for (int i = 0; i < matrix->size; i++) {
     for (int o = 0; o < matrix->size; o++) {
     }
   }
   return err;
 }
-float calculate_lenght(struct Object *obj_a,struct Object *obj_b)
+
+float calculate_lenght(struct Object *obj_a, struct Object *obj_b)
+
 {
   float lenght = -1;
   int a = obj_a->x - obj_b->x;
   a = abs(a);
-  
+
   return lenght;
 }
 
-int main (int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   if (argc != 3) {
     printf("Wrong number of arguments supplied\n");
     return err;
